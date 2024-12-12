f533e66affd8b68827cc9936c9e6192f8a8714b7
ali saghir <alisaghir543@gmail.com>
2024-12-12 17:46:28 +0400

    removed the script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Script from "next/script"; // Import the Script component
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,23 +30,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics Tracking Script using next/script */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-WF659TH2JG"
-          async
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-        >
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-WF659TH2JG');
-          `}
-        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
